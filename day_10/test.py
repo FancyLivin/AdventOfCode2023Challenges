57d09bc3fd0fb9461cd300cd380352e8d85d5d18
Yamaan Khan <ykhan4@asu.edu>
2024-01-12 00:33:15 -0500

    Finished day 10

--- day_10/test.py
+++ day_10/test.py
@@ -1,41 +1,59 @@
 import unittest
-import main as m
+import part_one as po
+import part_two as pt
 
 class TestPartOne(unittest.TestCase):
-    def setUp(self) -> None:
-        self.one = m.PartOne('test_one.txt')
+    def setUp(self):
+        self.one = po.PartOne('test_one.txt')
 
     def test_get_start_coords(self):
         self.assertEqual(self.one.start, (1, 1))
     
-    # @unittest.skip
     def test_get_farthest_point(self):
-        # farthest = self.one.get_farthest_point()
-        # self.assertEqual(farthest, 4)
+        farthest = self.one.get_farthest_point()
+        self.assertEqual(farthest, 4)
 
-        two = m.PartOne('test_two.txt')
-        farthest = two.get_farthest_point()
+        self.two = po.PartOne('test_two.txt')
+        farthest = self.two.get_farthest_point()
         self.assertEqual(farthest, 8)
-        # fails when going from (2,4) to (3,3) instead goes to (1,5)
-    
-    @unittest.skip
-    def test_move_cardinal_directions(self):
-        coords = self.one.change_coordinates('N', (1,1), (-1,-1), '|')
-        self.assertEqual(coords[0], (3, 1))
 
-        coords = self.one.change_coordinates('W', (3,1), (1,1), '-')
-        self.assertEqual(coords[0], (3, 3))
+    def test_move(self):
+       current = self.one.move((1,1))
+       self.assertEqual(current, (1,2))
 
-        coords = self.one.change_coordinates('S', (3,3), (3,1), '|')
-        self.assertEqual(coords[0], (1, 3))
+       current = self.one.move((1,2))
+       self.assertEqual(current, (1,3))
 
-        coords = self.one.change_coordinates('E', (1,3), (3,3), '-')
-        self.assertEqual(coords[0], (1, 1))
+       current = self.one.move((1,3))
+       self.assertEqual(current, (2,3))
 
-    @unittest.skip
-    def test_move_around_corners(self):
-        coords = self.one.change_coordinates('N', (1,2), (-1,-1), 'L')
-        self.assertEqual(coords[0], (2,3))
+       current = self.one.move((2,3))
+       self.assertEqual(current, (3,3))
 
+       current = self.one.move((3,3))
+       self.assertEqual(current, (3,2))
+
+       current = self.one.move((3,2))
+       self.assertEqual(current, (3,1))
+
+       current = self.one.move((3,1))
+       self.assertEqual(current, (2,1))
+
+       current = self.one.move((2,1))
+       self.assertEqual(current, (1,1))
+
+# Need to calculate area with vertices not with every point
 class TestPartTwo(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.two = pt.PartTwo('test_one.txt')
+    
+    def test_shoelace_formula(self):
+        self.two.navigate_pipes()
+        area = self.two.get_shoelace_formula()
+        self.assertEqual(area, 4)
+
+    def test_get_total_interior_points(self):
+        self.two.navigate_pipes()
+        area = self.two.get_shoelace_formula()
+        interior_points = self.two.get_total_interior_points(area)
+        self.assertEqual(interior_points, 1)
